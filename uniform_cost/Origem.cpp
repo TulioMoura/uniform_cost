f2f9ad4fc6c6cc67a06ec7402d5e450dea6c7b5d
Túlio <90994955+TulioMoura@users.noreply.github.com>
2023-05-01 18:18:11 -0300

    Refatorada e Criada Interface Básica, para pesquisar caminhos.

--- uniform_cost/Origem.cpp
+++ uniform_cost/Origem.cpp
@@ -13,76 +13,80 @@ using namespace std;
 //0 ORADEA -1 ZERIND -2 ARAD -3 SIBIU -4 FAGARAS -5 TIMISOARA -6 LUGOJ -7 RIMNICU VILCEA -
 //8 PITESTI -9 MEHADIA -10 DOBRETA -11 CRAIOVA -12 BUCHAREST -13 GIURGIU -14 URZICENI -15 HIRSOVA - 
 //16 EFORIE -17 VASLUI -18 LASI -19 NEAMT
-int map[n_cities][n_cities] = {
-	{ 0 ,71,0,151,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//oradea
-	{71,0,75,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//zerind
-	{0,75,0,140,0,118,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//arad
-	{151,0,140,99,0,0,0,80,0,0,0,0,0,0,0,0,0,0,0,0},//sibiu
-	{0,0,0,99,0,0,0,0,0,0,0,0,211,0,0,0,0,0,0,0},//fagaras
-	{0,0,118,0,0,0,111,0,0,0,0,0,0,0,0,0,0,0,0,0},//timisoara
-	{0,0,0,0,0,111,0,0,0,70,0,0,0,0,0,0,0,0,0,0},//lugoj
-	{0,0,0,80,0,0,0,0,97,0,0,146,0,0,0,0,0,0,0,0},//rimnicu vilcea
-	{0,0,0,0,0,0,0,97,0,0,0,138,101,0,0,0,0,0,0,0},//pitesti
-	{0,0,0,0,0,0,70,0,0,0,75,0,0,0,0,0,0,0,0,0},//mehadia
-	{0,0,0,0,0,0,0,0,0,75,0,120,0,0,0,0,0,0,0,0},//dobreta
-	{0,0,0,0,0,0,0,146,138,0,120,0,0,0,0,0,0,0,0,0},//craiova
-	{0,0,0,0,211,0,0,0,101,0,0,0,0,90,85,0,0,0,0,0},//bucharest
-	{0,0,0,0,0,0,0,0,0,0,0,0,90,0,0,0,0,0,0,0},//giurgiu
-	{0,0,0,0,0,0,0,0,0,0,0,0,85,0,0,98,0,142,0,0},//urziceni
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,98,0,86,0,0,0},//hirsova
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,86,0,0,0,0},//eforte
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,142,0,0,0,92,0},//vaslui
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,92,0,87},//lasi
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,87,0} }; // neamt
+
+string cities[20] = { "Oradea","Zerind","Arad","Sibiu","Fagaras","Timisoara","Lugoj","Rimnicu-Vilcea","Pitesti","Mehadia","Dobreta","Craiova","Bucharest","Giurgiu","Urziceni","Hirsova","Eforie","Valsui","Lasi","Neamt" };
+
 
 //precisarei calcular o custo do nó a cada iteração no array até encontrar o nó final
 struct path {
 public:
 	int cost;
 	int current;
-	string path_to;
-	path(int a, int b, string c) {
+	vector<int> path_to;
+	path(int a, int b, vector<int> c) {
 		cost = a; current = b; path_to = c;
 	}
 	bool operator<(path b) {
 		return cost < b.cost;
 	}
 };
+string convertPath(vector<int> a) {
+	string output;
+	for (int i = 0; i < a.size(); i++) {
+		output += (cities[a[i]]);
+		if (i < (a.size() - 1) ){
+			output+=" - ";
+		}
+	}
+	return output;
+}
 
+int findCityIndex(string c) {
+	for (int i = 0; i < n_cities; i++) {
+		if (c == cities[i]) {
+			return i;
+		}
+	}
+	return -1;
+}
 auto cmp = [](path a, path b) {return a.cost < b.cost; };
 
 path uniformCost(int source, int destination, int matrix[n_cities][n_cities]) {
 
 	vector<path> work_arr;
 	if (source == destination) {
-		return path(0, source, " " + to_string(source));
+		return path(0, source, {source});
 	}
 	else {
 		vector<int> visited;
-		work_arr.push_back({ 0,source," " + to_string(source)+" "});
+		work_arr.push_back({ 0,source,{source} });
 		while (1) {
-			for (int j = 0; j < work_arr.size(); j++) {
-				
-					
-					cout << work_arr[j].path_to <<"cost "<<work_arr[j].cost << " current: " << work_arr[j].current << endl;
-					
+			// Debug Only Line: 
+			/*for (int j = 0; j < work_arr.size(); j++) {
+				for (int k = 0; k < work_arr[j].path_to.size(); k++) {
+					cout << work_arr[j].path_to[k] << " ";
+				}
+
+				cout <<"cost "<<work_arr[j].cost << " current: " << work_arr[j].current << endl;
 			}
 			cout << "iteration" << endl;
+			*/
 			bool already_visited = false;
 			for (int j = 0; j < visited.size(); j++) {
 				if (visited[j] == work_arr[0].current) {
 					already_visited = true;
 					
 				}
-				cout << visited[j]<<" ";
+				//cout << visited[j]<<" ";
 			}
-			cout << "--------------------"<<endl;
+			//cout << "--------------------"<<endl;
 			if (already_visited == false) {
 				for (int i = 0; i < n_cities; i++) {
 					if (matrix[work_arr[0].current][i] > 0) {
-						{
-
-							work_arr.push_back({ matrix[work_arr[0].current][i] + work_arr[0].cost,	i,	(work_arr[0].path_to + to_string(i) + " ") });
+						{	
+							path temp = { matrix[work_arr[0].current][i] + work_arr[0].cost,	i,	work_arr[0].path_to };
+							temp.path_to.push_back(i);
+							work_arr.push_back(temp);
 							
 						}
 
@@ -126,8 +130,47 @@ path uniformCost(int source, int destination, int matrix[n_cities][n_cities]) {
 	
 
 int main() {
-	path result = uniformCost(3,13, map);
-	cout <<"caminho com menor custo" << result.path_to << endl;
-	cout << result.cost;
+	int map[n_cities][n_cities] = {
+	{ 0 ,71,0,151,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//oradea 0
+	{71,0,75,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//zerind    1
+	{0,75,0,140,0,118,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//arad   2
+	{151,0,140,0,99,0,0,80,0,0,0,0,0,0,0,0,0,0,0,0},//sibiu 3
+	{0,0,0,99,0,0,0,0,0,0,0,0,211,0,0,0,0,0,0,0},//fagaras  4
+	{0,0,118,0,0,0,111,0,0,0,0,0,0,0,0,0,0,0,0,0},//timisoara 5
+	{0,0,0,0,0,111,0,0,0,70,0,0,0,0,0,0,0,0,0,0},//lugoj      6
+	{0,0,0,80,0,0,0,0,97,0,0,146,0,0,0,0,0,0,0,0},//rimnicu vilcea 7
+	{0,0,0,0,0,0,0,97,0,0,0,138,101,0,0,0,0,0,0,0},//pitesti       8
+	{0,0,0,0,0,0,70,0,0,0,75,0,0,0,0,0,0,0,0,0},//mehadia          9
+	{0,0,0,0,0,0,0,0,0,75,0,120,0,0,0,0,0,0,0,0},//dobreta         10
+	{0,0,0,0,0,0,0,146,138,0,120,0,0,0,0,0,0,0,0,0},//craiova      11
+	{0,0,0,0,211,0,0,0,101,0,0,0,0,90,85,0,0,0,0,0},//bucharest    12
+	{0,0,0,0,0,0,0,0,0,0,0,0,90,0,0,0,0,0,0,0},//giurgiu           13
+	{0,0,0,0,0,0,0,0,0,0,0,0,85,0,0,98,0,142,0,0},//urziceni       14
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,98,0,86,0,0,0},//hirsova          15
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,86,0,0,0,0},//eforte            16
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,142,0,0,0,92,0},//vaslui          17
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,92,0,87},//lasi             18
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,87,0} }; // neamt         19
+	
+	bool exit = false;
+
+	while (!exit) {
+		string cityA, cityB;
+		int idxA=-1, idxB=-1;
+		while (idxA == -1 || idxB == -1) {
+			cout << "Insira o nome da cidade origem: ";
+			cin >> cityA;
+			cout << "Insira o nome da cidade destino: ";
+			cin >> cityB;
+
+			idxA = findCityIndex(cityA);
+			idxB = findCityIndex(cityB);
+		}
+		path result = uniformCost(idxA, idxB, map);
+		cout << "caminho com menor custo: " << endl << convertPath(result.path_to) << endl;
+		cout << "Custo do caminho: " << result.cost<<endl;
+		cout << "------------ " << endl << endl;
+
+	}
 	return 0;
 }
